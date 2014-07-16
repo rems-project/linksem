8bd5f663d4ad04df2a379544069d44656c3748ff
dpmulligan <dominic.p.mulligan@gmail.com>
2014-07-16 16:20:14 +0100

    more work

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,6 @@
 open Error
 open Pre_main
+open Elf
 
 let acquire_bitstring =
 	let path_to_target = "/usr/bin/ld" in
@@ -11,19 +12,7 @@ let acquire_bitstring =
 
 let _ =
 	let _ = Printf.printf "Starting...\n" in
-	let result =
-		acquire_bitstring >>= fun bs0 ->
-		Elf_header.read_elf32_elf_header bs0 >>= fun (elf_header, bs1) ->
-		let size, entry_size = Elf_header.program_header_table_size_and_entry_size elf_header in
-		Elf_program_header_table.read_elf32_program_header_table (size * entry_size) bs1 >>= fun (program_header_table, bs2) ->
-		  if Elf_header.elf32_elf_header_is_section_table_present elf_header then
-		    let size, entry_size, offset = Elf_header.section_header_table_size_and_entry_size_and_offset elf_header in
-		      Elf_section_header.read_elf32_section_header_table size entry_size offset bs0 >>= fun (section_header_table, bs3) ->
-		      	let _ = Printf.printf "%i\n" (List.length section_header_table) in
-		      		assert false
-		  else
-		    return program_header_table
-	in
+	let result = acquire_bitstring >>= read_elf32 in
 		match result with
-			| Fail err  -> Printf.printf "Fail: %s\n" err
-			| Success s ->	Printf.printf "%s\n" (display s)
+			| Fail err  -> Printf.printf "Fail\n%s\n" err
+			| Success s ->	Printf.printf "Success\n%s\n" (string_of_elf32 s)
