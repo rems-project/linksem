f295ae64981659ad0799ff92c1dcef871bb12faf
Peter Sewell <Peter.Sewell@cl.cam.ac.uk>
2014-10-30 17:29:41 +0000

    Kayvan, Dominic, Jean, Peter: making the ocamlbuild build build (also checking in a copy of the ppcmem2 src_lem_library so it can be in an ocamlbuild-approved subdirectory position)

--- src/_build/main.ml
+++ src/_build/main.ml
@@ -15,7 +15,6 @@ open Elf_executable_file2
 open Elf_executable_file3
 open Elf_linking_file2
 open Elf_linking_file3
-open Elf_linking_file4
 
 open Sail_interface
 
@@ -30,15 +29,14 @@ let default_sht_bdl =
 
 let _ =  
 (let res =    
-(Ml_bindings.acquire_bitstring "/usr/bin/ld" >>= (fun bs0 ->
-    Elf_executable_file3.read_elf32_executable_file3 bs0 >>= (fun f1 ->
-    let _ = (print_endline (Elf_executable_file3.string_of_elf32_executable_file3 default_hdr_bdl default_pht_bdl default_sht_bdl f1)) in
-    Elf_executable_file3.get_elf32_dynamic_symbol_table f1 >>= (fun symtab ->
-    let _ = (print_endline ("Symbol table length: " ^ string_of_int (List.length symtab))) in
-    let _ = (print_endline (Elf_symbol_table.string_of_elf32_symbol_table symtab)) in
-    Elf_executable_file3.get_elf32_section_header_string_table f1 >>= (fun strtab ->
-    Elf_symbol_table.get_elf32_symbol_image_address symtab strtab >>= (fun strs ->
-    let _ = (print_endline ("Length: " ^ (string_of_int (List.length strs)))) in
+(Ml_bindings.acquire_bitstring "test/power64-executable-2" >>= (fun bs0 ->
+    Elf_executable_file3.read_elf64_executable_file3 bs0 >>= (fun f1 ->
+    Elf_executable_file3.get_elf64_symbol_table f1 >>= (fun symtab ->
+    Elf_executable_file3.get_elf64_symbol_string_table f1 >>= (fun strtab ->
+    Elf_symbol_table.get_elf64_symbol_image_address symtab strtab >>= (fun strs ->
+    let _ = (print_endline (string_of_list 
+  (instance_Show_Show_tup2_dict instance_Show_Show_string_dict
+     instance_Show_Show_nat_dict) strs)) in
     return f1))))))
   in
     (match res with
