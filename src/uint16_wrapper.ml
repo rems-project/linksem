0aa317e3c9c89ce2a827e0b7f68a482987c42f0c
dpmulligan <dominic.p.mulligan@gmail.com>
2015-02-05 10:57:49 +0000

    started getting meaningful data out of elf header again rather than junk

--- src/uint16_wrapper.ml
+++ src/uint16_wrapper.ml
@@ -11,7 +11,18 @@ let of_bigint (i : Big_int.big_int) =
 let to_bigint (u : uint16) = u
 ;;
 
-let of_dual c1 c2 = assert false
+let shift_left i s =
+  Big_int.mod_big_int (Big_int.shift_left_big_int i s) max_int
+;;
+
+let shift_right i s =
+  Big_int.mod_big_int (Big_int.shift_right_big_int i s) max_int
+;;
+
+let of_dual c1 c2 =
+  let b1 = Big_int.big_int_of_int (Char.code c1) in
+  let b2 = shift_left (Big_int.big_int_of_int (Char.code c2)) 8 in
+    Big_int.add_big_int b1 b2
 ;;
 
 let to_bytes u = assert false
