30a1aa9bb3ccfe84e2da24a1852d6d3410ea853f
Dominic Mulligan <dominic.p.mulligan@gmail.com>
2015-11-14 10:46:53 +0000

    Thinking about proof...

--- prf/ELF_Relocation_Proof.thy
+++ prf/ELF_Relocation_Proof.thy
@@ -17,14 +17,38 @@ definition mov_constant_to_mem :: "64 word \<Rightarrow> 64 word \<Rightarrow> i
 definition mov_constant_from_mem :: "64 word \<Rightarrow> instruction" where
   "mov_constant_from_mem addr \<equiv> Zmov (Z_ALWAYS, (Z64, Zr_rm (RAX, Zm (None, (ZnoBase, addr)))))"
 
-definition program :: "8 word list" where
-  "program \<equiv>
-    List.concat (List.map encode [
+definition fixed_program :: "instruction list" where
+  "fixed_program \<equiv>
+    [
       mov_constant_to_mem (5 :: 64 word) (100 :: 64 word)
     , mov_constant_from_mem (100 :: 64 word)
-    ])"
+    ]"
+
+definition relocatable_program :: "8 word list" where
+  "relocatable_program \<equiv>
+     List.concat (List.map encode [
+       mov_constant_to_mem (5 :: 64 word) (0 :: 64 word)
+     , mov_constant_from_mem (0 :: 64 word)
+     ])"
 
 definition relocation_image :: "Abis.any_abi_feature annotated_memory_image" where
-  "relocation_image \<equiv> img1 program"
+  "relocation_image \<equiv> img1 relocatable_program"
+
+fun run_program :: "instruction list \<Rightarrow> X64_state \<Rightarrow> X64_state" where
+  "run_program [] \<sigma> = \<sigma>" |
+  "run_program (x#xs) \<sigma> =
+     (let (_, \<sigma>') = Run x \<sigma> in
+       run_program xs \<sigma>')"
+
+fun load_image :: "8 word list \<Rightarrow> X64_state" where
+  "load_image instrs = undefined"
+
+(* Theorem statement is something like:
+ *
+ * let ef = elf64_file_of_memory_image relocation_image in
+ * let im = obtain_executable_image ef in
+ * let \<sigma>0 = load_image im in
+ *   run_program fixed_program empty_state \<simeq> execute_two_steps \<sigma>0
+ *)
 
 end
