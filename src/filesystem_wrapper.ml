1071477e5085b7346782d8189eda544137667d63
emersion <contact@emersion.fr>
2018-07-24 16:02:58 +0100

    Make realpath resolve symlinks for parent dirs too

--- src/filesystem_wrapper.ml
+++ src/filesystem_wrapper.ml
@@ -10,23 +10,65 @@ let rec replace_last l v =
     | [_] -> [v]
     | h :: t -> h :: (replace_last t v)
 
-(* The OCaml's stdlib is retarded and doesn't have realpath *)
-let rec realpath p =
+let rec pop_last l =
+  match l with
+    | [] -> ([], None)
+    | [last] -> ([], Some last)
+    | h :: t ->
+      let (t, maybe_last) = pop_last t in
+      (h :: t, maybe_last)
+
+let readlink p =
   try
-    let target = Unix.readlink p in
-    let p = if String.get target 0 <> '/' then
-      let l = String.split_on_char '/' p in
-      let l = replace_last l target in
-      String.concat "/" l
-    else
-      target
-    in
-    realpath p
-  with Unix.Unix_error _ -> p
+    return (Unix.readlink p)
+  with Unix.Unix_error (code, name, arg) ->
+    Error.fail ("readlink: " ^ (Unix.error_message code))
+
+let is_abs_path p =
+  String.get p 0 = '/'
+
+let readlink_abs p =
+  match readlink p with
+    | Success target ->
+      let target =
+        if is_abs_path target then
+          target
+        else
+          let l = String.split_on_char '/' p in
+          let l = replace_last l target in
+          String.concat "/" l
+      in
+      return target
+    | Fail err ->
+      Error.fail err
+
+(* The OCaml's stdlib is retarded and doesn't have realpath *)
+(* TODO: this doesn't support relative paths *)
+let rec realpath p =
+  match readlink_abs p with
+    | Success p -> (
+      match realpath p with
+        | Success p -> (
+          let l = String.split_on_char '/' p in
+          let (l, maybe_last) = pop_last l in
+          match (l, maybe_last) with
+            | ([""], _) | (_, None) -> return p
+            | (_, Some filename) -> (
+              let parent = String.concat "/" l in
+              match realpath parent with
+                | Success parent -> return (parent ^ "/" ^ filename)
+                | Fail err -> Error.fail err
+              )
+          )
+        | Fail err -> Error.fail err
+      )
+    | Fail _ ->
+      (* TODO: this is ugly, but this is OCaml *)
+      return p
 
 let readdir dirname =
   try
     let a = Sys.readdir dirname in
     return (Array.to_list a)
   with Sys_error err ->
-    fail ("readdir: " ^ err)
+    Error.fail ("readdir: " ^ err)
